405d30762ce0137f39f88ebefb381712c4a7a40c
AndyFooGuoZhen <ndfucoding@gmail.com>
2022-06-26 11:40:03 -0500

    Updated ip for crud frontend

--- crud/src/http-common.js
+++ crud/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "http://172.30.187.211:8080/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
