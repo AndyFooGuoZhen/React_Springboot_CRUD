07d2ef99bf9bc1ef01962b0231b9234e6ee3ab6c
AndyFooGuoZhen <ndfucoding@gmail.com>
2022-06-28 21:20:26 -0500

    Resolved CORS related issue for frontend

--- crud/src/http-common.js
+++ crud/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://backend-trial-5-rit-smitra-lab-research.apps.nimbus.las.iastate.edu/api/v1",
+    "http://backend-trial-5-rit-smitra-lab-research.apps.nimbus.las.iastate.edu/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
