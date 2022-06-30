ecd5be64707fd46ad2056457f328a741f29a4d9f
AndyFooGuoZhen <ndfucoding@gmail.com>
2022-06-30 13:36:54 -0500

    Updated https link for frontend

--- crud/src/http-common.js
+++ crud/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "http://backend-andy-guozhen-dev.apps.sandbox.x8i5.p1.openshiftapps.com//api/v1",
+    "https://backend-andy-guozhen-dev.apps.sandbox.x8i5.p1.openshiftapps.com//api/v1",
   headers: {
     "Content-Type": "application/json",
   },
